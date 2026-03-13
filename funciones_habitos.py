d6beb7e8373c6ab0829bb9f5358b8c9d6881c56a
sofidavidov <sdavidov@udesa.edu.ar>
2026-03-13 16:06:27 -0300

    Update funciones_habitos.py

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -1,35 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 13 15:32:17 2026
-
-@author: manamoiseeff
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def analizar_habitos (lista):
      resultado = {} 
