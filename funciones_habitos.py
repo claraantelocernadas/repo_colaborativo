8abb58db4bdbe4d3209f0beaf8d2a5f0ff319223
mariamoiseeff <mmoiseeff@udesa.edu.ar>
2026-03-13 15:58:30 -0300

    Update funciones_habitos.py

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -1,8 +1,26 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 13 15:32:17 2026
 
-@author: manamoiseeff
-"""
+def registrar_habitos():
+    '''
+    Guarda en una lista las actividades que se le piden al usuario 
+    
+    Returns
+    -------
+    list: una lista de todas las actividades que haya ingresado el usuario
 
+    '''
+
+def analizar_habitos(lista): 
+    '''
+    Cuenta cuantas veces aparece cada actividad ingresada por el usuario
+
+    Parameters
+    ----------
+    lista : list
+        una lista de todas las actividades ingresadas por el usuario
+
+    Returns
+    -------
+    diccionario
+        diccionario contador de frecuencias de cuantas veces ingresa el usuario esa actividad
+
+    '''
